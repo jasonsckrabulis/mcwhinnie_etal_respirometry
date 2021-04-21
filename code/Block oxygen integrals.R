d247366543df5d88fc29b3032cccc732354a7894
Jason Sckrabulis <jason.sckrabulis@gmail.com>
2021-04-21 16:47:25 -0400

    Update Block oxygen integrals.R

--- code/Block oxygen integrals.R
+++ code/Block oxygen integrals.R
@@ -1,110 +1,105 @@
-# Integral calculations for oxygen measurements for an entire experimental block for TOTAL OXYGEN CONSUMPTION
+# Integral calculations for oxygen measurements for an entire experimental block for total oxygen consumption
 
-#make a vector of all filenames, full path names required to use output as loop
-###################
-#change initial file path to match G Drive location
-#run only the one needed for your analysis
-#each block needs to be filtered out with pattern="_". "NegDay", "ZeroDay", "OneDay", "FourDay", "EightDay"
-###################
+####
+# Make a vector of all filenames, full path names required to use output as loop
+# Change initial file path to match G Drive location and run only the one needed for your analysis
+# Each block can be filtered out as needed with pattern="_".
 
-#this one for OXYGEN
-#NEED TO CHANGE BOTH PATTERN AND FINAL FOLDER LOCATION TO GET PROPER DAY#####################################
-filename<-list.files(path="G:/My Drive/Raffel Lab Documents/2018-2019 Project Folders/2018-2019 X. Laevis Respirometry/X laevis Respiration/Experimental Data/Pulmonary Data/Detrended Oxygen/Block Five/Redo/",
-	pattern="Laevis",full.names=TRUE)
+# Oxygen example (Files organized into folders for each experimental block, no additional filter needed)
+filename <- list.files(path="PATH_TO_FOLDER", pattern="Laevis", full.names=TRUE)
 
-##filename<-list.files(path="G:/My Drive/Raffel Lab Documents/2018-19 Xenopus Experiments/X laevis Respiration/Experimental Data/Pulmonary Data/Detrended Oxygen/Integral Organization/NegDay/",
-##	full.names=TRUE)
-
-#confirms it pulled files out
+# Confirm it pulled files out
 filename
 
-#this one for BREATH BOUT
-#NEED TO CHANGE BOTH PATTERN AND FINAL FOLDER LOCATION TO GET PROPER DAY####################################
-filename<-list.files(path="G:/My Drive/Raffel Lab Documents/2018-19 Xenopus Experiments/X laevis Respiration/Experimental Data/Pulmonary Data/Breath Bout Data/Integral Organization/ZeroDay/",
-	pattern="ZeroDay",full.names=TRUE)
+#Breath bout example (Files also organized with day in filename: "NegDay", "ZeroDay", "OneDay", "FourDay", & "EightDay")
+filename <- list.files(path="PATH_TO_FOLDER", pattern="ZeroDay", full.names=TRUE)
 
-#confirms it pulled files out
+# Confirms it pulled files out
 filename
 
-#function to calculate all integrals
-#arguments:
-#filename: path to the file of interest
-#type: "oxygen" for output for overall O2 Percent change, "breaths" for output for individual breaths O2 Percent and flow rate
-
-RespIntegrals<-function(filename,type){
+# Function to calculate all integrals
+# Arguments:
+# filename: path to the file of interest
+# type: type="oxygen" for output for overall O2 Percent change, type="breaths" for output for individual breaths O2 Percent and flow rate
+RespIntegrals <- function(filename, type){
+	
+	# Determine if type = oxygen calculation
 	if(type == "oxygen"){
-		#read csv and subset first datapoint out (it has interval=0)
-		dat<-read.csv(file=filename,header=TRUE)
-		dat<-subset(dat,interval!=0)
 		
-		#total time of respiration
-		totalTimeSec<-max(dat$Seconds)
+		# Read csv and subset first datapoint out (it has interval=0)
+		dat <- read.csv(file=filename, header=TRUE)
+		dat <- subset(dat, interval!=0)
+		
+		# Get total time of respiration
+		totalTimeSec <- max(dat$Seconds)
 	
-		#subset negative baseline and raw
-		negBase<-subset(dat,correctedBaseO2<=0)
-		negRaw<-subset(dat,correctedRawO2<=0)
+		# Subset negative baseline and raw values
+		negBase <- subset(dat, correctedBaseO2<=0)
+		negRaw <- subset(dat, correctedRawO2<=0)
 	
-		#subset positive baseline and raw
-		posBase<-subset(dat,correctedBaseO2>0)
-		posRaw<-subset(dat,correctedRawO2>0)
+		# Subset positive baseline and raw values
+		posBase <- subset(dat, correctedBaseO2>0)
+		posRaw <- subset(dat, correctedRawO2>0)
 	
-		#negative integral
-		negBaseInt<-sum(negBase$correctedBaseO2*negBase$interval)
-		negRawInt<-sum(negRaw$correctedRawO2*negRaw$interval)
-		negTotalO2<-(abs(abs(negRawInt)-abs(negBaseInt)))/100
+		# Calculate negative integral
+		negBaseInt <- sum(negBase$correctedBaseO2*negBase$interval)
+		negRawInt <- sum(negRaw$correctedRawO2*negRaw$interval)
+		negTotalO2 <- (abs(abs(negRawInt) - abs(negBaseInt)))/100
 	
-		#positive integral
-		posBaseInt<-sum(posBase$correctedBaseO2*posBase$interval)
-		posRawInt<-sum(posRaw$correctedRawO2*posRaw$interval)
-		posTotalO2<-abs(abs(posRawInt)-abs(posBaseInt))
+		# Calculate positive integral
+		posBaseInt <- sum(posBase$correctedBaseO2*posBase$interval)
+		posRawInt <- sum(posRaw$correctedRawO2*posRaw$interval)
+		posTotalO2 <- abs(abs(posRawInt) - abs(posBaseInt))
 	
-		#baseline check
-		baseCheck<-abs(abs(negBaseInt)-abs(posBaseInt))
+		# Check baseline approximates 0
+		baseCheck <- abs(abs(negBaseInt) - abs(posBaseInt))
 	
-		#output
-		output<-c(totalTimeSec,negBaseInt,posBaseInt,baseCheck,negRawInt,negTotalO2)
+		# Output integral calculations
+		output <- c(totalTimeSec, negBaseInt, posBaseInt, baseCheck, negRawInt, negTotalO2)
 		output
-		
+	
+	# Otherwise breath calculations
 	} else if (type == "breaths"){
-		#read csv and subset first datapoint out (it has interval=0)
-		dat<-read.csv(file=filename,header=TRUE)
-		dat<-subset(dat,interval!=0)
 		
-		#total time of respiration
-		totalTimeSec<-max(dat$Seconds)
+		# Read csv and subset first datapoint out (it has interval=0)
+		dat <-read.csv(file=filename, header=TRUE)
+		dat <- subset(dat, interval!=0)
 		
-		#start time in milliseconds
-		startMillis<-min(dat$Millis)
+		# Get total time of respiration
+		totalTimeSec <- max(dat$Seconds)
+		
+		# Get start time in milliseconds
+		startMillis <- min(dat$Millis)
 	
-		#end time in milliseconds
-		endMillis<-max(dat$Millis)
+		#Get end time in milliseconds
+		endMillis <- max(dat$Millis)
 	
-		#PERCENT OXYGEN
-		#subset negative baseline and raw
-		negBaseO2<-subset(dat,corBaseO2<=0)
-		negRawO2<-subset(dat,corRawO2<=0)
+		#Percent oxygen calculations
+		# Subset negative baseline and raw values
+		negBaseO2 <- subset(dat, corBaseO2<=0)
+		negRawO2 <- subset(dat, corRawO2<=0)
 	
-		#subset positive baseline and raw
-		posBaseO2<-subset(dat,corBaseO2>0)
-		posRawO2<-subset(dat,corRawO2>0)
+		# Subset positive baseline and raw values
+		posBaseO2 <- subset(dat, corBaseO2>0)
+		posRawO2 <- subset(dat, corRawO2>0)
 	
-		#negative integral
-		negBaseIntO2<-sum(negBaseO2$corBaseO2*negBaseO2$interval)
-		negRawIntO2<-sum(negRawO2$corRawO2*negRawO2$interval)
-		negTotalO2<-(abs(abs(negRawIntO2)-abs(negBaseIntO2)))/100
+		#Calculate negative integral
+		negBaseIntO2 <- sum(negBaseO2$corBaseO2*negBaseO2$interval)
+		negRawIntO2 <- sum(negRawO2$corRawO2*negRawO2$interval)
+		negTotalO2 <- (abs(abs(negRawIntO2) - abs(negBaseIntO2)))/100
 	
-		#positive integral
-		posBaseIntO2<-sum(posBaseO2$corBaseO2*posBaseO2$interval)
-		posRawIntO2<-sum(posRawO2$corRawO2*posRawO2$interval)
-		posTotalO2<-abs(abs(posRawIntO2)-abs(posBaseIntO2))
+		# Calculate positive integral
+		posBaseIntO2 <- sum(posBaseO2$corBaseO2*posBaseO2$interval)
+		posRawIntO2 <- sum(posRawO2$corRawO2*posRawO2$interval)
+		posTotalO2 <- abs(abs(posRawIntO2) - abs(posBaseIntO2))
 	
-		#baseline check
-		baseCheckO2<-abs(abs(negBaseIntO2)-abs(posBaseIntO2))
+		# Check baseline approximtes 0
+		baseCheckO2 <- abs(abs(negBaseIntO2) - abs(posBaseIntO2))
 	
-		#FLOW RATE
-		#subset negative baseline and raw
-		negBaseFlow<-subset(dat,corBaseFlow<=0)
-		negRawFlow<-subset(dat,corRawFlow<=0)
+		#Flow rate calculations
+		# Subset negative baseline and raw values
+		negBaseFlow <- subset(dat, corBaseFlow<=0)
+		negRawFlow <- subset(dat, corRawFlow<=0)
 		
 		#subset positive baseline and raw
 		posBaseFlow<-subset(dat,corBaseFlow>0)
